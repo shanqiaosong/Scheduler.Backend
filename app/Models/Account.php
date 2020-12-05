284cfa97524ea6f115eade4b992865f1b966e86a
Zhihan Shan <shanzh@pku.edu.cn>
2020-12-05 15:55:03 +0800

    fix primary key bug

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -10,6 +10,9 @@ class Account extends Model
 {
     use HasFactory;
     protected $primaryKey = 'student_id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
     function setCredentialAttribute($credential){
         $this->attributes['credential'] = Crypt::encryptString($credential);
     }
